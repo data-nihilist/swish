d50f83f973cdca14f9bf905ca1326fcc80938c64
data-nihilist <mattmccredy@gmail.com>
2024-01-18 00:03:59 -0500

    histogram progress, minor styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <div className="navbar-mint">
-        <h1 className="display-f text-white">SWISH</h1>
+        <h1 className="display-f text-white">SWISH<code className="ml-1">dashboard</code></h1>
       </div>
         <Dashboard/>
       </div>
